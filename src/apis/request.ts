2e4397ce3c1d9c3398db0a3ff0a456becd6e5ad5
HerveyB3B4 <zhwaaaaa@outlook.com>
2025-01-28 17:28:44 +0800

    chore(apis)

---
+++ src/apis/request.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const request = (config: object) => {
+  const instance = axios.create({
+    timeout: 1000
+  });
+
+  instance.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")!}`;
+
+  return instance(config);
+};
+
+export default request;
