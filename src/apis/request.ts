fd3601c1c3218f0910ac9346291369041db93004
HerveyB3B4 <zhwaaaaa@outlook.com>
2025-01-29 01:58:23 +0800

    fix(apis): update request.ts.

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -1,13 +1,53 @@
 import axios from "axios";
 
-const request = (config: object) => {
-  const instance = axios.create({
-    timeout: 1000
-  });
+const api = axios.create({
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
 
-  instance.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")!}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-  return instance(config);
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    if (error.response) {
+      console.error("Response error:", error.response);
+    } else if (error.request) {
+      console.error("Request error:", error.request);
+    } else {
+      console.error("Error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+const request = {
+  get: (url: string, params = {}) => api.get(url, { params }),
+  post: (url: string, data = {}) => api.post(url, data),
+  postFormData: (url: string, formData = {}) =>
+    api.post(url, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }),
+  put: (url: string, data = {}) => api.put(url, data),
+  patch: (url: string, data = {}) => api.patch(url, data),
+  delete: (url: string, data = {}) => api.delete(url, { data })
 };
 
 export default request;
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig, loadEnv } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
-const env = loadEnv('development', process.cwd(), '');
+const env = loadEnv("development", process.cwd(), "");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src")
     }
   },
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: env.VITE_HOST,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, "")
       }
     }
   }
-})
+});
