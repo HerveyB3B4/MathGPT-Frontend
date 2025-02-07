53b5977cda45ab5a0c554b21ac7a3a27ffc22ec5
dioptric233 <13567713788@163.com>
2025-02-07 15:51:24 +0800

    feat:增加了侧边栏组件对话页面组件和侧边模块占位块

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,7 @@
 <template>
-  <header class="bg-white-600 text-white shadow-md fixed top-0 w-full z-50">
-    <div class="container mx-auto flex items-center justify-between p-4">
-      <div class="text-2xl font-bold">
-        Math Solver
-      </div>
-      <div class="hidden md:flex space-x-6">
-        主页
-      </div>
-      <div class="flex space-x-4">
-        <button
-          @click="handleLogin"
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors"
-        >
-          登录
-        </button>
-        <button
-          @click="handleLogout"
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors"
-        >
-          注销
-        </button>
-        <button
-          @click="handleSignUp"
-          class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded transition-colors"
-        >
-          注册
-        </button>
-      </div>
+    <div >
+      <router-view />
     </div>
-  </header>
-  <router-view />
 </template>
 
 <script setup lang="ts">
@@ -38,17 +10,6 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const handleLogin = () => {
-  router.push('/login');
-};
-
-const handleSignUp = () => {
-  router.push('/signup');
-};
-
-const handleLogout = () => {
-  router.push('/signup');
-};
 </script>
 
 <style scoped>
